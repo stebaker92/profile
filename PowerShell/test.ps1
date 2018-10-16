584cafd0a119f2fb05918eac8c5d6c8d1c17d9cc
Steve Baker <stebaker92@users.noreply.github.com>
2018-10-16 14:12:01 +0100

    Update test.ps1

--- PowerShell/test.ps1
+++ PowerShell/test.ps1
@@ -1,1 +1,11 @@
 echo "Test String"
+
+Add-Type -AssemblyName System.speech
+
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+$speak.Speak('Hello...')
+
+Remove-Item alias:cd
+
+cls
